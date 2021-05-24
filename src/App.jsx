c337788d04b4a41330582e4ac7b496a521155afc
mifikaji <82213199+mifikaji@users.noreply.github.com>
2021-05-24 13:35:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,61 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-const Profile =()=>{
-  return <h1>This is profile's page</h1>
-}
-const Messages =()=>{
-  return <h1>This is your messages</h1>
-}
-const Seyyings =()=>{
-  return <h1>Settings</h1>
-}
-const Friends =()=>{
-  return <h1>Find your friends here</h1>
-}
-const Menu =()=>{
-  return 
-}
+const Profile = () => {
+	return <h1>Это страница с профилем</h1>;
+};
+
+const Settings = () => {
+	return <h1>Это страница с настройками</h1>;
+};
+const Messages = () => {
+	return <h1>Это страница с сообщениями</h1>;
+};
+const Friends = () => {
+	return <h1>друзья</h1>;
+};
+const Menu = () => {
+	return (
+		<div>
+			<div className='nav flex-column nav-pills' aria-orientation='vertical'>
+				<a className='nav-link active' href='profile'>
+					Профиль
+				</a>
+				<a className='nav-link' href='messages'>
+					Сообщения
+				</a>
+				<a className='nav-link' href='settings'>
+					Настройки
+				</a>
+				<a className='nav-link' href='friends'>
+					Мои друзья
+				</a>
+			</div>
+		</div>
+	);
+};
+
 function App() {
-  return
-  <div className="container-fluid">
-    <BrowserRouter>
-    <div class="row">
-      <div className="col-sm-3">
-       <Menu/>
-      </div>
-    
-      <div className="col-sm-9">
-      <BrowserRouter>
-      <Route path='/profile' component {Profile}/>
-      <Route path='/messages' component {Messages}/>
-      <Route path='/settings' component {Settings}/>
-      <Route path='/friends' component {Friends}/>
-      </BrowserRouter>
-      </div>
-    </div>
-    </BrowserRouter>
-  </div>
+	return (
+		<div className='container-fluid'>
+			<div className='row'>
+				<div className='col-sm-3'>
+					<Menu />
+				</div>
+
+				<div className='col-sm-9'>
+					<BrowserRouter>
+						<Route path='/profile' component={Profile} />
+						<Route path='/messages' component={Messages} />
+						<Route path='/settings' component={Settings} />
+						<Route path='/friends' component={Friends} />
+					</BrowserRouter>
+				</div>
+			</div>
+		</div>
+	);
 }
+
 export default App;
